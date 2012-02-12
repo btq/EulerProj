0e0c1ea184a3ee6f4a73bedf9703b7c19eecc254
Brian Quinn <btquinn@gmail.com>
2012-02-11 23:12:41 -0500

    completed EP009

--- src/EP009_PythTripletSumsTo1000.cpp
+++ src/EP009_PythTripletSumsTo1000.cpp
@@ -3,12 +3,28 @@
 using namespace std;
 
 /*A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
-a2 + b2 = c2
+a^2 + b^2 = c^2
 
-For example, 32 + 42 = 9 + 16 = 25 = 52.
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
 */
 
-
+int main(int argc, char** argv)
+{
+	int a,b,c;
+	for (b=2; b<500; b++)
+	{
+		a=(1000*(500-b))/(1000-b);
+		c = sqrt( (a*a) + (b*b) );
+		cout << "a=" << a << " b=" << b << " c=" << c << " sum=" << a+b+c << endl;
+		if ((1000-a-b-c) == 0)
+		{
+			cout << "Found a=" << a << " b=" << b << " c=" << c << endl;
+			cout << "Answer is " << a*b*c << endl;
+			return 0;
+		}
+	}
+	return 0;
+}	
